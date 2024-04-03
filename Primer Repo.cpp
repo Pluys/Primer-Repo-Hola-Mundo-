3a241d90f6fa9bb57126be108073a3ba7a413651
Dekina <martinjeremias223@gmail.com>
2024-04-03 10:50:46 -0300

    juanelcaballo

---
+++ Primer Repo.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main(){
+declare numero
+cin>> numero;
+if (numero>5)
+{
+cout<< "mayor que cinco";
+}
+else
+{
+cout<< "menor que cinco";
+}
+return 0;
+}
